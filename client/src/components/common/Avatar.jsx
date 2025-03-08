0f8b9339f3e78134918795e03f16e00e26cbec29
SuhasPalani <sugipals007@gmail.com>
2025-03-07 20:55:51 -0600

    implementation of photo

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -1,9 +1,24 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import { FaCamera } from "react-icons/fa";
+import ContextMenu from "./ContextMenu";
 
 function Avatar({ type, image, setImage }) {
   const [hover, setHover] = useState(false);
+  const [isContextMenuVisible, setIsContextMenuVisible] = useState(false);
+  const [contextMenuCordinates, setContextMenuCordinates] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const showContextMenu = (e) => {
+    e.preventDefault();
+    setIsContextMenuVisible(true);
+    setContextMenuCordinates({ x: e.clientX, y: e.clientY });
+  };
+
+  const contextMenuOptions = [{ name: "Take Photo", callback: () => {} }];
+
   return (
     <>
       <div className="flex items-center justify-center">
@@ -27,9 +42,19 @@ function Avatar({ type, image, setImage }) {
               className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center rounded-full justify-center flex-col text-center gap-2
             ${hover ? "visible" : "hidden"}
             `}
+              onClick={(e) => showContextMenu(e)}
+              id="context-opener"
             >
-              <FaCamera className="text-2xl" if="context-opener" />
-              <span>Change Profile Photo</span>
+              <FaCamera
+                className="text-2xl"
+                id="context-opener"
+                onClick={(e) => showContextMenu(e)}
+              />
+              <span onClick={(e) => showContextMenu(e)} id="context-opener">
+                Change <br />
+                Profile <br />
+                Photo
+              </span>
             </div>
             <div className="flex items-center justify-center h-60 w-60 ">
               <Image src={image} alt="avatar" className="rounded-full" fill />
@@ -37,6 +62,14 @@ function Avatar({ type, image, setImage }) {
           </div>
         )}
       </div>
+      {isContextMenuVisible && (
+        <ContextMenu
+          options={contextMenuOptions}
+          coordinates={contextMenuCordinates}
+          contextMenu={isContextMenuVisible}
+          setContextMenu={setIsContextMenuVisible}
+        />
+      )}
     </>
   );
 }
--- client/src/components/common/ContextMenu.jsx
+++ client/src/components/common/ContextMenu.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 
-function ContextMenu() {
-  return <div>ContextMenu</div>;
+function ContextMenu({ options, coordinates, contextMenu, setContextMenu }) {
+  const contextMenuRef = useRef(null);
+  const hancleClick = (e, callback) => {};
+  return (
+    <div
+      className="bg-dropdown-background fixed py-2 z-[100] shadow-xl"
+      style={{
+        top: `${coordinates.y}px`,
+        left: `${coordinates.x}px`,
+      }}
+      ref={contextMenuRef}
+    >
+      <ul>
+        {options.map(({ name, callback }) => (
+          <li key={name} onClick={(e) => hancleClick(e, callback)}
+            className="px-5 py-2 hover:bg-background-default-hover cursor-pointer">
+            <span className="text-white">{name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ContextMenu;
